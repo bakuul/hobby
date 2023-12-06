2232baa32104f4949bb0af2317fbe51d2afee1e0
Your Name <you@example.com>
2023-12-06 18:37:55 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 0,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  
